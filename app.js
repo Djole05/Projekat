02c60511bc4a55fab4435a867675a420a843a643
Djole05 <mit0505@yahoo.com>
2017-06-08 17:06:22 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,61 @@
+window.onload = function() {
+  
+  var dugmePosalji = document.querySelector("#forma");
+  dugmePosalji.addEventListener("submit", PosaljiBlog);
+
+}
+
+function AjaxZahtev(options, callback) {
+  var req = new XMLHttpRequest();
+  req.open(options.metod, options.putanja, true);
+  req.addEventListener("load", function() {
+    if (req.status < 400) {
+      console.log(req);
+ 		  callback(req.responseText);
+    }
+    else {
+ 		  callback(new Error("Request failed: " + req.statusText));
+    }
+  });
+  req.addEventListener("error", function() {
+    callback(new Error("Network error"));
+  });
+  req.send(options.sadrzaj);
+
+}
+
+
+
+function PosaljiBlog(e){
+  e.preventDefault();
+
+	/*alert("Blog se salje.")*/
+  var bloger = document.querySelector("#name").value;
+  var email = document.querySelector("#email").value;
+  var naslov = document.querySelector("#title").value;
+  var blog = document.querySelector("#message").value;
+ // var date = document.querySelector("#date").value;
+  var options = {}
+  options.metod = "post";
+  options.putanja  = "noviBlog";
+  var poruka = {"autor":bloger, "email":email, "naslov":naslov, "blog":blog}
+  options.sadrzaj = JSON.stringify(poruka); 
+  AjaxZahtev(options, PrikaziOdgovorNaPoruku)
+}
+
+  function PrikaziOdgovorNaPoruku(odgovor){
+  var odgovor2 = JSON.parse(odgovor);
+
+  var tekst = "<section> <h1>" + odgovor2.naslov + "</h1>";
+  //document.getElementById("blogovi").innerHTML +=  "<h3>" + odgovor2.autor + "</h3>";
+  tekst += "<h3>" + odgovor2.autor + "</h3>";  
+  tekst += "<p>" + odgovor2.blog + "</p>"; 
+  tekst += "<h6>" + odgovor2.email + "</h6> </section>"
+  document.getElementById("blogovi").innerHTML = tekst + document.getElementById("blogovi").innerHTML;
+ 
+ /* alert(document.querySelector("#blogovi").innerHTML)
+  var prikazi = document.querySelector("#blogovi");
+  prikazi.innerHTML = odgovor; */
+
+
+}
---
+++ server.js
@@ -0,0 +1,92 @@
+var http = require("http");
+var fs = require("fs");
+var svi_blogovi=[]
+
+// Kesiranje statickih fajlova
+var pocetna = fs.readFileSync("index.html","utf8");
+var appcss = fs.readFileSync("app.css","utf8");
+var appjs = fs.readFileSync("app.js","utf8");
+var ikona = fs.readFileSync("favicon.ico");
+var pozadina = fs.readFileSync("background.jpg");
+
+function prikaziPocetnuStranu(response){
+  response.writeHead(200, {"Content-Type": "text/html"});
+  fs.readFile("blogovi.txt", "utf8", function(error, text) {
+       if (error)
+          return;
+        svi_blogovi = JSON.parse(text);
+        var blogovi =""
+        for(i=svi_blogovi.length-1;i>=0;i--){
+          naslov = svi_blogovi[i].naslov
+          autor = svi_blogovi[i].autor
+          blog = svi_blogovi[i].blog
+          email = svi_blogovi[i].email
+          blogovi += "<section> <h1>" + naslov + "</h1>"
+          blogovi += "<h3>" + autor + "</h3>";  
+          blogovi += "<p>" + blog + "</p>"; 
+          blogovi += "<h6>" + email + "</h6> </section>"
+        }
+        pocetna = pocetna.replace("#blogovi#", blogovi)
+  });
+  response.end(pocetna);
+} 
+
+function nepoznatURL(response){
+  response.writeHead(200, {"Content-Type": "text/html"});
+  response.write("<h1>Not Found</h1>");
+  response.end();
+}
+
+function UpisNaDisk(blogovi){
+  fs.writeFile("blogovi.txt", blogovi, function(err) {
+        if (err)
+          console.log("Failed to write file:", err);
+        else
+          console.log("File written.");
+});
+}
+
+function OdgovorNaZahtev(request,response){
+  //console.log(request.url)
+
+  switch(request.url) {
+    case "/": 
+    case "/index.html": 
+              prikaziPocetnuStranu(response);
+              break;
+    case "/app.css":
+              response.writeHead(200, {"Content-Type": "text/css"});
+              response.end(appcss);
+              break;
+    case "/background.jpg":
+              response.writeHead(200, {'Content-Type': 'image/jpg' });
+              response.end(pozadina, 'binary');
+              break;
+    case "/favicon.ico":
+              response.writeHead(200, {'Content-Type': 'image/gif' });
+        			response.end(ikona, 'binary');
+              break;
+    case "/app.js":
+						  response.writeHead(200, {"Content-Type": "text/plain"});
+	            response.end(appjs);
+	            break;
+    case "/noviBlog":
+             // response.end("Primio sam novi blog.");
+              request.setEncoding('utf8');
+              request.on('data', function (novi_blog) {
+                
+                svi_blogovi.push(JSON.parse(novi_blog));
+                UpisNaDisk(JSON.stringify(svi_blogovi));
+                console.log(svi_blogovi);
+                response.end(novi_blog);
+              });
+              break;
+      default: 
+	            nepoznatURL(response);
+	            break;
+	}
+}
+
+var server = http.createServer(OdgovorNaZahtev);
+server.listen(8007);
+console.log("Server ceka zahteve na portu 8007");
